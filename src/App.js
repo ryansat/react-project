2419cefa8d715a5e2c6f25875a633719d24e58a0
Fajar Satria <ryansat46@gmail.com>
2019-10-23 14:31:01 +0700

    init

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Test from './Test'
+import './App.css';
+
+export default function App() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul className="App-ul">
+            <li  className="App-li">
+              <a >  <Link to="/" className="App-link">Home</Link> </a>
+            </li>
+            <li className="App-li">
+              <a >  <Link to="/about" className="App-link">About</Link> </a>
+            </li>
+            <li className="App-li">
+            <a >  <Link to="/portofolio" className="App-link">Portofolio</Link> </a>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/portofolio">
+            <Portofolio />
+          </Route>
+          <Route path="/">
+            <Test />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+function Home() {
+  return <h1>Home</h1>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Portofolio() {
+  return <h2>My Portofolio</h2>;
+}
